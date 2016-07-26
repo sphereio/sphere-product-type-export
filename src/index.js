ac9d621d63d7048ae47454a8d3dab1172ab10f43
philippspo <philipp.sporrer@commercetools.de>
2016-07-26 19:18:25 +0200

    refactor(index): remove unused code and add start script

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
 import 'babel-polyfill'
 import ProductTypeExport from './product-type-export'
-// import { getSphereClientCredentials } from './utils'
-
-// const PROJECT_KEY = 'toom-pre-staging-38'
-// const logger = {
-//   trace: console.log,
-//   debug: console.log,
-//   info: console.log,
-//   error: console.error,
-// }
-//
-// getSphereClientCredentials(PROJECT_KEY)
-// .then(sphereCredentials => {
-//   const options = {
-//     config: sphereCredentials,
-//   }
-//
-//   const productTypeExport = new ProductTypeExport(
-//     logger,
-//     { sphereClientConfig: options }
-//   )
-//   productTypeExport.run().then(() => console.log('done done'))
-// })
 
 export default ProductTypeExport
